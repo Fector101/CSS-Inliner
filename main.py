676e26238d4b293ecd85ea2526b427094fc3ee5b
Fector101 <fabianjoseph063@gmail.com>
2024-09-12 18:33:09 +0100

    Alot of features

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
+# ADD ability to remove empty selectors (style_section)
+
 unique_char_4_space="defiohf;qebio;gofhwe9-fpweffopefo"
-with open('input/main.css', mode='r') as data:
+with open('input/small.css', mode='r') as data:
     file_code=data.read()
     CODE=file_code
-    # list_of_group_styles=file_code.replace('\n','').split('}')
-    # print(list_of_group_styles)
-# for each in list_of_group_styles:
-#     print('pp'+each+'}','\n')
-# import string
-def removeAllFromList(input:str,list_:list):
+    # print(CODE.count('\n'))
+  
+def replaceAllFromList(input:str,list_:list,char_to_replace_with:str):
     if input == '' or input == unique_char_4_space:
         return ''
     output=unique_char_4_space
     for each in list_:
         # output = unique_char_4_space 
         if(output==unique_char_4_space):
-            output=input.replace(each,'')
+            output=input.replace(each,char_to_replace_with)
         else:
-            output=output.replace(each,'')
+            output=output.replace(each,char_to_replace_with)
     return output
-def removeComments(code):
+def removeComments(code:str):
     if '/*' not in code:
         return code
     new_str=''
     found_comment_end=True
     i = 0
+    if code.count('/*') != code.count('*/'):
+
+        ...
     for char in code:
         if char == '/' and i+1 != len(code) and code[i+1] == '*':
             found_comment_end = False
@@ -37,48 +39,232 @@ def removeComments(code):
         return ''
     else:
         return new_str
+def writeInFile(str_=''):
+    with open('ouput/without_whitespace/style.css',mode='w')as file:
+        file.write(str_)
+def myStrip(code:str):
+    new_str=''
+    i=0
+    remove_space=False
+    # code=replaceAllFromList(repr(code),[r'\t',r'\v',r'\n',r'\r',r'\f'],'')#.replace('\n','')
+    # code=repr(code)
+    code=code.replace('\n','')
+    # code=removeComments(code)
+    # print(code[0:30])
+    # code=re.sub(r'[^\S\t\f\v\r\n]+ ','',code)
+    lenght_of_str=len(code)
+    checkpoints=['{',':',';','}']
+    for char in code:
+        if any(i == char for i in checkpoints):
+            remove_space=True
+            if char=='{':
+                new_str=new_str.rstrip() #removing space between h1 above open curlly braces e.g "h1 {"
+            new_str+=char
+        elif (char == '/' and i+1 != lenght_of_str and code[i+1] == '*') or (char == '*' and i-1 != 0 and code[i-1] == '/'):#/*
+            # print(char, code[i+1], code[i+2], code[i+3], code[i+4], code[i+5], code[i+6], code[i+7], code[i+8], code[i+9], code[i+10], code[i+11], code[i+12])
+            new_str+=char
+            remove_space=True
+        # elif (char == '*' and i+1 != len(code) and code[i+1] == '/'):
+        elif (char == '*' and i+1 != lenght_of_str and code[i+1] == '/') or (char == '/' and i-1 != 0 and code[i-1] == '*'):
+            new_str+=char
+            remove_space=True
+        elif char == ' ' and remove_space:
+            pass
+        else:# and found_style_start:
+            # if new_str and any(new_str[-1] == i for i in ['{',';']):
+            #     new_str+='\t'+char
+            # else:
+            remove_space=False
+            new_str+=char
+        i+=1
+    return new_str
 
 def removeWhiteSpaces():
-    remove_whitespaces=CODE.replace('\n','')#.replace('\n','')
-    # print(remove_whitespaces+'ppp')
-    with open('ouput/without_whitespace/style.css',mode='w')as file:
-        file.write('')
-    list_of_styles=[]
+    no_comments=removeComments(CODE)    # Doesn't make sense to have comments when no whitespaces it won't be visible
+    #ADD Function to remove empty selectors (style_section)
+    no_whitespaces=myStrip(no_comments)
+
+    with open('ouput/without_whitespace/style.css','w')as file:
+        file.write(no_whitespaces)
+
+# removeWhiteSpaces()
+def stylesToObject(code:str):
+    """Works when comments are removed withy myStrip
+    and semi-column added when a selector has another selector within
+    """
+    code_=myStrip(code)
+    code_=removeComments(code_)
+    
+    list_of_selectors=[]
+    styles={}
+    found_selector_end=False # found end of selector e.g "hr","h1","div", ".class-name","#id"
+    found_a_style_name_start=False
+    found_a_style_name_end=False # i.e ":"
+    selector=''
+    style_des_name='' #'background-color'
+    style_des_value='' #'#FF0AAA'
+    new_selector=''
+    found_a_style_value_start=False
+    # found_a_style_value_end=False # i.e ";" or '}'
+    found_animation=False
+    # current_selecto
+    # def start4newSelector():
+    #     global found_end,style_des_name, style_des_value,selector
+    #     found_end=False
+    #     style_des_name=''
+    #     style_des_value=''
+    #     selector=''
+    all=''
+    for char in code_:
+        # print(found_end,found_a_style_name_end,found_a_style_name_start,found_a_style_value_start)
+        # if found_end == True and char == '{':
+          #  ...
+        # print(char)
+        if found_selector_end == False and char !='{':
+            selector+=char
+            # print(111)
+        elif found_selector_end == False and char == '{':
+            styles[selector]={}
+            found_selector_end=True
+            # print(222)
+        elif char == '{' and found_selector_end == True:    # for animations and selctor with parent selector
+            # """
+            #     e.g this will get the new selector 'h1 when it spilts
+            #     "color: red; display: flex; h1".split(';')
+            #     div{
+            #         color: red;
+            #         display:flex;
+            #         h1{
+            #             text-algin:center;
+            #         }
+            #     }
+            # """
+            # print(style_des_name)
+            found_styles=';'.join(style_des_name.split(';')[0:-1])  #returns -->> e.g color: red; display: flex; and takes out h1
+            styles[selector][style_des_name]=found_styles
+            new_selector=style_des_name.split(';')[-1].lstrip()
+            # print(new_selector,'---',style_des_value.split(';'))
+            style_des_value=''
+            style_des_name=''
+            found_a_style_name_end=False
+            styles[selector][new_selector]={}
+
+        elif found_selector_end == True and found_a_style_name_end == False and char != ':': # added (and each != ':') to move (elif found_a_style_name_start and each == ':':) section after this elif statement:
+            style_des_name+=char
+            found_a_style_name_start=True
+            # print(333)
+        elif found_a_style_name_start and char == ':':
+            found_a_style_name_end=True
+        elif found_a_style_name_end and char != ';' and char != '}':
+            style_des_value+=char
+            found_a_style_value_start=True
+            # print(555)
+        elif found_a_style_name_end and found_a_style_value_start and (char == ';' or char == '}'):
+            # print(666)
+            found_a_style_name_end=False
+            # print('test')
+            # print(new_selector,'||',style_des_name,'||',style_des_value)
+            if new_selector:
+                styles[selector][new_selector][style_des_name]=style_des_value
+            else:
+                styles[selector][style_des_name]=style_des_value
+            style_des_name=''
+            style_des_value=''
+        
+        if found_selector_end and char == '}' : # if statement so it can come here if last written style does not have ";" and it's captured by `(char == ';' or (char == '}'))`
+            if 0==1:#'{' in style_des_value and (style_des_value.count('{') != style_des_value.count('}')):  # for animations and selctor with parent selector
+                pass
+            else:
+                if new_selector:
+                    new_selector=''
+                else:    
+                    found_selector_end=False
+                    selector=''
+                style_des_name=''
+                style_des_value=''
+    return styles
+    # writeInFile(str(styles))
+    # print(styles)
+    # for each in styles:
+    #     print(each)
+    #     list_of_selectors.append(each)
+    # print(list_of_selectors)
+    # list_of_styles=[]
+    # for each in styles.values():
+    #     list_of_styles.append(each)
+    # print(list_of_styles)
+# stylesToObject(CODE)
+def removeEmptySelectors(code:str):
+    styles_obj=stylesToObject(code)
+    style=''
+    for key, value in styles_obj.items():
+        # style+=key+{
+        print(key,'||',value)
+        
+        for e,e1 in value.items():
+            print(e,e1)
+
+removeEmptySelectors(CODE)
+# print(';'.join("color: red; display: flex; h1".split(';')[0:-1]))
+# print(';'.join("color: red display: flex h1".split(';')[0:-1]))
+
+# d={'rr':'aa','gg':'bb','ff':'cc'}
+# print('\n')
+# for e in d.values():
+#     print(e)
+# p='o'
+# print(p!=';' and p!='{')
+def formatNicely(code):
+    """Also removes empty empty selectors :) just a feature code runs 100% without it"""
+    # new_str=CODE.replace('\n','').replace(';',';\n').replace('}','}\n')
+    # writeInFile(removeComments(new_str))
+    new_str=myStrip(code)
+    # new_str=CODE.replace('{','{\r')#
+    str_=''
+    lenght_of_str = len(new_str)
     i=0
-    style_sections=removeComments(remove_whitespaces).split('}')
-    for style_section in style_sections:#remove_whitespaces.split('}')[0:-1]:
-        if i+1==len(style_sections) and style_section =='':
-            pass
+    for char in new_str:
+        if (char == '*' and i+1 != lenght_of_str and new_str[i+1] == '/') or (char == '/' and i-1 != 0 and new_str[i-1] == '*'):
+            if char == '/':
+                str_+=char+'\n\t'
+            else:
+                str_+=char
+        elif char=='}':
+            # print(len(new_str))
+            if i>2 and new_str[i-1]=='/' and new_str[i-2] =='*':
+                # print(str_[-2])
+                str_=str_[0:-2] +'\n'+char+'\n'
+                # print(999)
+                # str_+=char+'\n'
+            else:
+                str_+='\n'+char+'\n'
+        elif str_ and any(str_[-1] == i for i in ['{',';']):
+            str_+='\n\t'+char
+            # print(str_)
         else:
-            style_section=removeComments(style_section)
-            formatted=style_section+'}'
-            print('|||',formatted)
-            list_of_styles.append(formatted)
+            str_+=char
         i+=1
+    # new_str=str_.replace('}','}\n').replace('{','{\n').replace('*/','*/\n').replace(';',';\n')#.replace(';',';\n')
+    # new_str=str_#.replace('}','}\n')#.replace(';',';\n')#.replace(';',';\n')
+    # new_str=str_.replace('}','}\n').replace('{','{\n')#.replace(';',';\n')#.replace(';',';\n')
 
-    for style_section in list_of_styles:
-        with open('ouput/without_whitespace/style.css',mode='a')as file:
-            file.write(style_section)
+    writeInFile(str_)#[1:-1]
 
-removeWhiteSpaces()
-# print('ss'.)
-# print(40%2)
-# for each in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16][0:-1:2]:
-#     print(each)
-# print('pw/*pwk/*')
- # if '/*' in style_section:
-            #     new_str=''
-            #     found_comment_end=True
-            #     # if style_section.count('/*') != style_section.count('*/'): #Checking if commented correctly
-            #     #     print('Comments not formatted correctly')
-            #     #     return
-            #     i = 0
-            #     for char in style_section:
-            #         if char == '/' and i+1 != len(style_section) and style_section[i+1] == '*':
-            #             found_comment_end = False
-            #         elif (char == '*' and i+1 != len(style_section) and style_section[i+1] == '/') or (char == '/' and i-1 != 0 and style_section[i-1] == '*'):
-            #             found_comment_end=True
-            #         elif found_comment_end:
-            #             new_str+=char
-            #         i+=1
-            #     style_section=new_str
+# formatNicely(CODE)
+# print('poph'[0:-1])
+#  if found_end and char == '}' : # if statement so it can come here if last written style does not have ";" and it's captured by `(char == ';' or (char == '}'))`
+#             # if statment so current selector and values don't eraser if it's an animations or selctor with parent selector
+#             # print(found_end)
+#             if char=='}'and'{' in style_des_value and (style_des_value.count('{') != style_des_value.count('}')):  # for animations and selctor with parent selector
+#                 # print(33,style_des_value)
+#                 all+=char
+#                 #(style_des_value.count('{') != style_des_value.count('}')) checks if done with the child selectors
+#                 style_des_value+='}'
+#                 # found_animation=True
+#                 pass
+#             elif char=='}' and '{' in style_des_value and (style_des_value.count('{') == style_des_value.count('}')):  # for animations and selctor with parent selector
+#                 print(888)
+#                 styles[selector][style_des_name]=style_des_value
+#                 start4newSelector()
+#             else:
+#                 start4newSelector()
